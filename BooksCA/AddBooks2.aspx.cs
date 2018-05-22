149535f121787fef878a6fa87fe90612f5df4187
James Wang <shijun.james@gmail.com>
2018-05-22 15:42:18 +0800

    adding page books for user

---
+++ BooksCA/AddBooks2.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.IO;
+using System.Data.OleDb;
+
+namespace BooksCA
+{
+    public partial class AddBooks2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
